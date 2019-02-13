f63046351a168451ccc1ed8f8950f5f79b71e0a9
doltank9 <46945482+doltank9@users.noreply.github.com>
2019-02-13 13:54:03 +0900

    Create EX01_addMotor.ino

---
+++ Practice/EX01_addMotor.ino
@@ -0,0 +1,28 @@
+//Source Code
+//modified from example "addMotor "
+#include <DynamixelShield.h>
+DynamixelShield dxl;
+  
+void setup() {
+  // put your setup code here, to run once:
+  dxl.begin(57600);
+  delay(1000);
+  dxl.addMotor(1,  M_XL430);
+  dxl.torqueOn(1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+  if (dxl.getDxlCount() > 0)
+  {
+    dxl.setGoalAngle(1,  0);
+    delay(1000);
+    dxl.setGoalAngle(1,  900);
+    delay(1000);
+    dxl.setGoalAngle(1,  0);
+    delay(1000);
+    dxl.setGoalAngle(1, -900);
+    delay(1000);    
+  }
+}
