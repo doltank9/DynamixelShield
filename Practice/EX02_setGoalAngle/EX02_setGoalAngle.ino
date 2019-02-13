66c45f05075ae9ea195429ea620d8275a7a5cb06
doltank9 <46945482+doltank9@users.noreply.github.com>
2019-02-13 14:01:22 +0900

    Create EX02_setGoalAngle.ino

---
+++ Practice/EX02_setGoalAngle/EX02_setGoalAngle.ino
@@ -0,0 +1,27 @@
+//Source Code
+//modified from example "goalPosition" & "goalAngle"
+// by doltank9
+#include <DynamixelShield.h>
+DynamixelShield dxl;
+  
+void setup() {
+  // put your setup code here, to run once:
+  dxl.begin(57600);
+  delay(1000);
+  dxl.addMotor(1,M_XL430);
+  dxl.torqueOff(DXL_ALL_ID);
+  dxl.setJointMode(1);
+  dxl.torqueOn(DXL_ALL_ID);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dxl.setGoalAngle(1,-1800);
+  delay(1000);
+  for(int i=-1800;i<=1800;i+=450)
+  {
+    dxl.setGoalAngle(1, i );
+    delay(500);
+  }
+  delay(1000);
+}
