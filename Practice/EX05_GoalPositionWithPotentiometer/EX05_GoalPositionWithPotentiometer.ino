62e7490c98bdc09e8716f36e73ed417a4a6dabbb
doltank9 <46945482+doltank9@users.noreply.github.com>
2019-02-14 13:46:11 +0900

    Update and rename Practice/EX05_GoalPositionWithJoystick/EX05_GoalPositionWithJoystick.ino to Practice/EX05_GoalPositionWithPotentiometer/EX05_GoalPositionWithPotentiometer.ino

--- Practice/EX05_GoalPositionWithPotentiometer/EX05_GoalPositionWithPotentiometer.ino
+++ Practice/EX05_GoalPositionWithPotentiometer/EX05_GoalPositionWithPotentiometer.ino
@@ -1,5 +1,5 @@
-// Source Code
-// modified from example "goalPosition" & "goalAngle"
+//소스코드
+// by doltank9
 
 #include <DynamixelShield.h>
 DynamixelShield dxl;
@@ -12,11 +12,12 @@ void setup() {
   dxl.torqueOff(DXL_ALL_ID);
   dxl.setJointMode(1);
   dxl.torqueOn(DXL_ALL_ID);
-  pinMode(15,INPUT);
+  pinMode(15, INPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  dxl.setGoalPosition(1,1000+analogRead(15)*2);
+  int pos = map(analogRead(15), 0, 1023, -1800, 1800);
+  dxl.setGoalAngle(1, pos);
   delay(20);
 }
