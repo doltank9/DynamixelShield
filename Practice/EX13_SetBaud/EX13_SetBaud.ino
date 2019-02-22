ffde371c61cf1ab4dc917a64d6ec422be1597e96
doltank9 <46945482+doltank9@users.noreply.github.com>
2019-02-22 11:36:33 +0900

    Create EX13_SetBaud.ino

---
+++ Practice/EX13_SetBaud/EX13_SetBaud.ino
@@ -0,0 +1,27 @@
+.ino//소스코드
+#include <DynamixelShield.h>
+DynamixelShield dxl;
+
+void setup() {
+  // put your setup code here, to run once:
+  dxl.begin(1000000);
+  delay(1000);
+  dxl.addMotor(2,M_XL430);
+  dxl.torqueOff(DXL_ALL_ID);
+  dxl.setID(0xFE,2);
+  dxl.reset(2);
+  delay(1000);
+  dxl.begin(57600);
+  delay(1000);
+  dxl.addMotor(1,M_XL430);
+  dxl.setJointMode(1);
+  dxl.torqueOn(1);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  dxl.setGoalAngle(1,-450);
+  delay(1000);
+  dxl.setGoalAngle(1,450);
+  delay(1000);
+}
